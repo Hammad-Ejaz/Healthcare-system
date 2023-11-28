78ba5191bc05485ba5ac178afdf087e8f81be100
Hammad Ejaz <hammadejaz2021@gmail.com>
2023-11-28 23:03:12 +0500

    Update doctor search in DoctorSelection
    Closes #3

--- HealthCare/HealthCare.UI/Pages/DoctorSelection.razor.cs
+++ HealthCare/HealthCare.UI/Pages/DoctorSelection.razor.cs
@@ -3,6 +3,7 @@ using HealthCare.Data.Entity;
 using HealthCare.Service.IService;
 using HealthCare.ViewModels;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace HealthCare.UI.Pages
 {
@@ -19,12 +20,48 @@ namespace HealthCare.UI.Pages
 		[Parameter]
 		public string SearchText { get; set; }
 		public List<DoctorViewModel> Doctors { get; set; }
+        public int col_1 { get; set; } = 0;
+        public int col_2 { get; set; } = 0;
+        public int col_3 { get; set; } = 0;
+        public int col_4 { get; set; } = 0;
 		
+
 		protected override async Task OnInitializedAsync()
 		{
 			Doctors = await DoctorService.GetDoctorBySearchText(SearchText);
-		}
-        private async Task DoctorSearch()
+			AssignColumnsValues(Doctors.Count());
+
+        }
+		public void AssignColumnsValues(int count)
+		{
+            if (count < 0)
+            {
+                throw new ArgumentException("Input value must be non-negative.");
+            }
+            if (count == 1)
+            {
+                col_1 = 1;
+            }
+            else if (count == 2)
+            {
+                col_1 = 1;
+                col_2 = 1;
+            }
+            else if (count == 3)
+            {
+                col_1 = 1;
+                col_2 = 1;
+                col_3 = 1;
+            }
+            else
+            {
+                col_1 = (int)Math.Ceiling(count * 0.25);
+                col_2 = (int)Math.Ceiling(count * 0.50);
+                col_3 = (int)Math.Ceiling(count * 0.75);
+                col_4 = count;
+            }
+        }
+	    private async Task DoctorSearch()
         {
 			// You can customize the URL based on your requirements
 		 	await OnInitializedAsync();
